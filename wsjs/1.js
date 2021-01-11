62870584e3e26cd629083054220b1f763f08170f
Maygician <maygicinc@gmail.com>
2021-01-11 20:00:29 +0700

    some interactivity

--- wsjs/1.js
+++ wsjs/1.js
@@ -1,59 +1,171 @@
-var phx = function (){
-    let socket = new Socket("127.0.0.1:4000/socket", {params: {userToken: "123"}})
-socket.connect()
-let channel = socket.channel("room:123", {token: roomToken})
-channel.on("new_msg", msg => console.log("Got message", msg) )
-$input.onEnter( e => {
-  channel.push("new_msg", {body: e.target.val}, 10000)
-    .receive("ok", (msg) => console.log("created message", msg) )
-    .receive("error", (reasons) => console.log("create failed", reasons) )
-    .receive("timeout", () => console.log("Networking issue...") )
-})
+// Show an element
+var show = function (elem) {
+	elem.style.display = 'flex';
+};
+var show_block = function (elem) {
+	elem.style.display = 'block';
+};
 
-channel.join()
-  .receive("ok", ({messages}) => console.log("catching up", messages) )
-  .receive("error", ({reason}) => console.log("failed join", reason) )
-  .receive("timeout", () => console.log("Networking issue. Still waiting..."))}
-  var tst = function(){
-console.log("tst")
+// Hide an element
+var hide = function (elem) {
+	elem.style.display = 'none';
+};
+
+// Toggle element visibility
+var toggle = function (elem) {
+
+	// If the element is visible, hide it
+	if (window.getComputedStyle(elem).display === 'flex') {
+		hide(elem);
+		return;
+	}
+
+	// Otherwise, show it
+	show(elem);
+
+};
+// Toggle element visibility
+var toggle_block = function (elem) {
+
+	// If the element is visible, hide it
+	if (window.getComputedStyle(elem).display === 'block') {
+		hide(elem);
+		return;
+	}
+
+	// Otherwise, show it
+	show_block(elem);
+
+};
+
+// #region
+window.dataTable = function () {
+	return {
+	  items: [],
+	  view: 5,
+	  searchInput: '',
+	  pages: [],
+	  offset: 5,
+	  pagination: {
+		total: data.length,
+		lastPage: Math.ceil(data.length / 5),
+		perPage: 5,
+		currentPage: 1,
+		from: 1,
+		to: 1 * 5
+	  },
+	  currentPage: 1,
+	  sorted: {
+		field: 'name',
+		rule: 'asc'
+	  },
+	  initData() {
+		this.items = data.sort(this.compareOnKey('name', 'asc'))
+		this.showPages()
+	  },
+	  compareOnKey(key, rule) {
+		return function(a, b) { 
+		  if (key === 'name' || key === 'job' || key === 'email' || key === 'country') {
+			let comparison = 0
+			const fieldA = a[key].toUpperCase()
+			const fieldB = b[key].toUpperCase()
+			if (rule === 'asc') {
+			  if (fieldA > fieldB) {
+				comparison = 1;
+			  } else if (fieldA < fieldB) {
+				comparison = -1;
+			  }
+			} else {
+			  if (fieldA < fieldB) {
+				comparison = 1;
+			  } else if (fieldA > fieldB) {
+				comparison = -1;
+			  }
+			}
+			return comparison
+		  } else {
+			if (rule === 'asc') {
+			  return a.year - b.year
+			} else {
+			  return b.year - a.year
+			}
+		  }
+		}
+	  },
+	  checkView(index) {
+		return index > this.pagination.to || index < this.pagination.from ? false : true
+	  },
+	  checkPage(item) {
+		if (item <= this.currentPage + 5) {
+		  return true
+		}
+		return false
+	  },
+	  search(value) {
+		if (value.length > 1) {
+		  const options = {
+			shouldSort: true,
+			keys: ['name', 'job'],
+			threshold: 0
+		  }                
+		  const fuse = new Fuse(data, options)
+		  this.items = fuse.search(value).map(elem => elem.item)
+		} else {
+		  this.items = data
+		}
+		// console.log(this.items.length)
+		
+		this.changePage(1)
+		this.showPages()
+	  },
+	  sort(field, rule) {
+		this.items = this.items.sort(this.compareOnKey(field, rule))
+		this.sorted.field = field
+		this.sorted.rule = rule
+	  },
+	  changePage(page) {
+		if (page >= 1 && page <= this.pagination.lastPage) {
+		  this.currentPage = page
+		  const total = this.items.length
+		  const lastPage = Math.ceil(total / this.view) || 1
+		  const from = (page - 1) * this.view + 1
+		  let to = page * this.view
+		  if (page === lastPage) {
+			to = total
+		  }
+		  this.pagination.total = total
+		  this.pagination.lastPage = lastPage
+		  this.pagination.perPage = this.view
+		  this.pagination.currentPage = page
+		  this.pagination.from = from
+		  this.pagination.to = to
+		  this.showPages()
+		}
+	  },
+	  showPages() {
+		const pages = []
+		let from = this.pagination.currentPage - Math.ceil(this.offset / 2)
+		if (from < 1) {
+		  from = 1
+		}
+		let to = from + this.offset - 1
+		if (to > this.pagination.lastPage) {
+		  to = this.pagination.lastPage
+		}
+		while (from <= to) {
+		  pages.push(from)
+		  from++
+		}
+		this.pages = pages
+	  },
+	  changeView() {
+		this.changePage(1)
+		this.showPages()
+	  },
+	  isEmpty() {
+		return this.pagination.total ? false : true
+	  }
+	}
   }
+// #endregion
 
-    var gql2 = function(){
-var xd = document.getElementById('xdat');
-// xd.innerHTML="";
-xd.setAttribute("x-data","{}");
-
-// Event.stopPropagation()
-{/* <div @my-custom-event.stop="handleIt()"> */}
-{/* <button x-data @click="$dispatch('hey-fetch-the-data')"></button> */}
-
-
-    }
-
-//     // Select the node that will be observed for mutations
-// const targetNode = document.getElementById('some-id');
-
-// // Options for the observer (which mutations to observe)
-// const config = { attributes: true, childList: true, subtree: true };
-
-// // Callback function to execute when mutations are observed
-// const callback = function(mutationsList, observer) {
-//     // Use traditional 'for loops' for IE 11
-//     for(const mutation of mutationsList) {
-//         if (mutation.type === 'childList') {
-//             console.log('A child node has been added or removed.');
-//         }
-//         else if (mutation.type === 'attributes') {
-//             console.log('The ' + mutation.attributeName + ' attribute was modified.');
-//         }
-//     }
-// };
-
-// // Create an observer instance linked to the callback function
-// const observer = new MutationObserver(callback);
-
-// // Start observing the target node for configured mutations
-// observer.observe(targetNode, config);
-
-// // Later, you can stop observing
-// observer.disconnect();
